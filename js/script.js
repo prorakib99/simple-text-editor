ea826c5cdf3a76722d6b830a15d58491f08daa6e
Your Name <you@example.com>
2023-11-21 13:20:48 +0600

    Final Commit

--- js/script.js
+++ js/script.js
@@ -14,21 +14,16 @@
     });
     document.getElementById('align-left').addEventListener('click', function(){
         textField.style.textAlign = 'left';
-        this.classList.toggle('font-bold');
     });
     document.getElementById('align-center').addEventListener('click', function(){
         textField.style.textAlign = 'center';
-        this.classList.toggle('font-bold');
     });
     document.getElementById('align-right').addEventListener('click', function(){
         textField.style.textAlign = 'right';
-        this.classList.toggle('font-bold');
     });
     document.getElementById('align-justify').addEventListener('click', function(){
         textField.style.textAlign = 'justify';
-        this.classList.toggle('font-bold');
     });
-
     document.getElementById('font-size').addEventListener('click', function(){
         let fontValue = this.value;
         textField.style.fontSize = `${fontValue}px`;
@@ -39,6 +34,31 @@
     })
     document.getElementById('color-value').addEventListener('blur', function(){
         const colorValue = this.value;
-        console.log(colorValue);
         textField.style.color = colorValue;
     })
+    
+    // This one for Handle align Propertise 
+    let activeIndex = null;
+    const alignItems = document.getElementById('align-control');
+
+    function handleListClick(event) {
+    const clickedItem = event.target;
+
+    // Check if a list item was clicked
+    if (clickedItem.classList.contains('item-click')) {
+        const currentIndex = Array.from(clickedItem.parentNode.children).indexOf(clickedItem);
+
+        // Remove active class from the currently active item
+        if (activeIndex !== null) {
+        clickedItem.parentNode.children[activeIndex].classList.remove('font-bold');
+        }
+
+        // Toggle active class for the clicked item
+        clickedItem.classList.toggle('font-bold');
+
+        // Update the active index
+        activeIndex = clickedItem.classList.contains('font-bold') ? currentIndex : null;
+    }
+    }
+
+  alignItems.addEventListener('click', handleListClick);
